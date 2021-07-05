e2ecdd80172d89bad48ae09424cbcea9b91c6afb
Yom Woldemichael <yom904@gmail.com>
2021-07-05 11:57:09 -0400

    removed sidepanel and moved navbar and connect to homescreen

--- client/src/containers/HomeScreen.tsx
+++ client/src/containers/HomeScreen.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import Partitions from './HomeScreen/TopicsDisplay/Partitions';
 // Home screen renders two componenets - the left panel (Sidepane) and the right topics display (topicsDisplay)
 
-// importing Sidepane component
-import Sidepanel from './HomeScreen/Sidepanel';
+// -- Update -- I removed the side panel component and moved Navbar and connect to be part of Homescreen
 
 // importing TopicsDisplay
 import TopicsDisplay from './HomeScreen/TopicsDisplay';
 
+// importing Navbar
+import NavBar from './HomeScreen/Sidepanel/NavBar';
+
+// importing connect
+import Connect from './HomeScreen/Sidepanel/Connect';
+
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
-      <Sidepanel />
+
+      <NavBar />
+      <Connect />
+      {/* the table showing topics - partitions shown onClick */}
+      <hr></hr>
       <TopicsDisplay />
       <Partitions />
     </div>
